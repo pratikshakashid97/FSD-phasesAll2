13c60e905b5aa41b34946e59d66268f28169ad0f
pratikshakashid97 <pratikshakashid97@gmail.com>
2022-03-05 17:12:52 +0530

    Assignment

---
+++ Assiested Project/FilterDemop/src/main/java/com/simplilearn/filter/InfoServlet.java
@@ -0,0 +1,31 @@
+package com.simplilearn.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfoServlet
+ */
+@WebServlet("/Info")
+public class InfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.print("this is my InfoServlet");
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
